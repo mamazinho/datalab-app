f18b118129acbe2a17fe5749fca2113a3267ef49
mamazinho <matheusot2000@gmail.com>
2025-10-07 00:15:15 -0300

    componentize list chats

---
+++ src/components/Chats/list-chats.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { DatalabAPI, type IRetrieveChat } from '../../services/datalab-api';
+
+export const AllChats = () => {
+    const [chats, setChats] = useState<IRetrieveChat[]>([]);
+
+    const fetchChats = () => {
+        DatalabAPI.getAllChats().then(data => {
+            console.log("data", data);
+            setChats(data);
+        });
+    };
+
+    useEffect(() => {
+        fetchChats();
+    }, []);
+
+    return (
+        <div className="mt-4">
+            <h3>Chats Disponíveis</h3>
+            <div className="list-group">
+                {chats.map(chat => (
+                    <div key={chat.id}>
+                        <Link to={`/chats/${chat.id}/messages`} className="list-group-item list-group-item-action">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">{chat.title}</h5>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
--- src/pages/List-Chats/list-chats.tsx
+++ src/pages/List-Chats/list-chats.tsx
@@ -1,41 +1,12 @@
-import { Link } from 'react-router-dom';
 import { ChatContainer } from './list-chats.style';
-import { useEffect } from 'react';
-import { datalabClient } from '../../services/datalab-api';
+import { AllChats } from '../../components/Chats/list-chats';
+import { CreateChat } from '../../components/Chats/create-chat';
 
 export const ListChats = () => {
-  useEffect(() => {
-    datalabClient().get('/chats').then(response => {
-      console.log("Chats disponíveis:", response.data);
-    });
-  }, []);
-
   return (
     <ChatContainer>
-      <div>
-        <button className="btn btn-primary">Criar chat!</button>
-      </div>
-
-      <div className="mt-4">
-        <h3>Chats Disponíveis</h3>
-        <div className="list-group">
-          <Link to="/chats/1/messages" className="list-group-item list-group-item-action">
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">Chat Geral #1</h5>
-              <small>Ativo</small>
-            </div>
-            <p className="mb-1">Chat para perguntas gerais e assistência.</p>
-          </Link>
-          
-          <Link to="/chats/2/messages" className="list-group-item list-group-item-action">
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">Chat Técnico #2</h5>
-              <small>Ativo</small>
-            </div>
-            <p className="mb-1">Chat especializado em questões técnicas.</p>
-          </Link>
-        </div>
-      </div>
+      <CreateChat />
+      <AllChats />
     </ChatContainer>
   );
 };
