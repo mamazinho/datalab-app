5d1681225bf7a70c252fc5ba3e83af7c4fc14bf7
matheus.tessaroli <matheus.tessaroli@olist.com>
2025-10-07 00:15:15 -0300

    componentize list chats

---
+++ src/components/Chats/create-chat.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { DatalabAPI, type ICreateChat } from '../../services/datalab-api';
+
+export const CreateChat = () => {
+    const [toCreateChat, setToCreateChat] = useState(false);
+    const [chatPayload, setChatPayload] = useState<ICreateChat>();
+
+    const createChat = (payload?: ICreateChat) => {
+        if (!payload) throw new Error("Payload is required");
+        DatalabAPI.createChat(payload).then(data => {
+            setChatPayload(data);
+        });
+    };
+
+    const handleCreateChat = () => {
+        createChat(chatPayload);
+        setToCreateChat(false);
+        setChatPayload(undefined);
+    }
+
+    return (
+        <div>
+            <button className="btn btn-primary" onClick={() => setToCreateChat(!toCreateChat)}>Create Chat</button>
+            {toCreateChat && (
+                <div>
+                    <input
+                        type="text"
+                        value={chatPayload?.title}
+                        onChange={e => setChatPayload({ title: e.target.value })}
+                        placeholder="Chat Title"
+                    />
+                    <button type="submit" onClick={handleCreateChat}>Create</button>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/services/datalab-api/index.ts
+++ src/services/datalab-api/index.ts
@@ -1,13 +1,37 @@
 import axios from "axios";
 
-export const datalabClient = (token?: string, contentType = 'application/json') => {
-  return axios.create({
-    baseURL: import.meta.env.VITE_DATALAB_API_URL,
-    headers: {
-      xsrfCookieName: 'csrftoken',
-      xsrfHeaderName: 'X-CSRFToken',
-      'Content-Type': contentType,
-      Authorization: token ? `Bearer ${token}` : false,
-    },
-  });
+const axionsInstance = axios.create({
+  baseURL: import.meta.env.VITE_DATALAB_API_URL,
+  headers: {
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
+    'Content-Type': 'application/json',
+  },
+});
+
+export interface IRetrieveChat {
+    id: number;
+    user_id: number;
+    title: string;
+    input_tokens: number;
+    output_tokens: number;
+    number_of_requests: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface ICreateChat {
+    title: string;
+}
+
+export const DatalabAPI = {
+  async getAllChats(): Promise<IRetrieveChat[]> {
+    const response = await axionsInstance.get('v1/chats')
+    return response.data as IRetrieveChat[];
+  },
+
+  async createChat(payload: ICreateChat): Promise<IRetrieveChat> {
+    const response = await axionsInstance.post('v1/chats', payload);
+    return response.data as IRetrieveChat;
+  }
 };
