dc6286e09b03e3d2a2b12dd1c6b649a878f8050b
mamazinho <matheusot2000@gmail.com>
2025-10-14 20:51:47 -0300

    start styling

---
+++ src/lib/components/layouts/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="bg-white shadow dark:bg-gray-800 rounded-2xl">
+    <div class="container flex items-center justify-between mx-auto">
+        <a href="/"><img class="w-auto h-6 sm:h-7" src="favicon.ico" alt="Logo da DataLab" /></a>
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+            
+            <a href="/chats" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Chats</a>
+        </div>
+    </div>
+</nav>
