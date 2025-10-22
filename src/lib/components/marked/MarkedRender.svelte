8adacd21335406bbb764800e0c159dbdd223835e
matheus.tessaroli <matheus.tessaroli@olist.com>
2025-10-22 00:46:43 -0300

    add style and components to messages, organize messages correctly

---
+++ src/lib/components/marked/MarkedRender.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { marked } from 'marked';
+    import DOMPurify from 'dompurify';
+
+    let { content }: { content: string } = $props();
+
+    marked.setOptions({
+        breaks: true,
+        gfm: true,
+    });
+
+    let rawHTML = marked.parse(content);
+    let sanitizedHTML = DOMPurify.sanitize(rawHTML as string);
+    
+</script>
+
+{@html sanitizedHTML}
