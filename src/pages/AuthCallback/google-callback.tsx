f5ab1510cf983aeaef03e3f650ab64057c037e90
matheus.tessaroli <matheus.tessaroli@olist.com>
2026-02-10 10:27:56 -0300

    simplify google oauth flows and

--- src/pages/AuthCallback/google-callback.tsx
+++ src/pages/AuthCallback/google-callback.tsx
@@ -1,46 +1,44 @@
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import type { ILoginUserResponse } from '../../services/datalab-api/authResource';
+import type { ISocialLoginCallbackEvent } from '../Login/login';
+
 
 export const GoogleCallback: React.FC = () => {
     const [searchParams] = useSearchParams();
 
     useEffect(() => {
         const accessToken = searchParams.get('access_token');
+        if (!accessToken) {
+            console.error("Nenhum token encontrado na URL de retorno.");
+            return;
+        }
         const expiresIn = searchParams.get('expires_in');
         const scope = searchParams.get('scope');
         const tokenType = searchParams.get('token_type');
 
-        if (accessToken) {
-            // Usa BroadcastChannel para comunicação robusta e isolada
-            const authChannel = new BroadcastChannel('auth_channel');
-            
-            authChannel.postMessage({
-                type: 'GOOGLE_LOGIN_SUCCESS',
-                response: {
-                    access_token: accessToken,
-                    expires_in: expiresIn ? parseInt(expiresIn) : 3600,
-                    scope: scope || 'read write',
-                    token_type: tokenType || 'Bearer',
-                } as ILoginUserResponse,
-            });
-
-            authChannel.close();
-
-            // Fallback & Cleanup
-            setTimeout(() => {
-                window.close();
-                if (!window.closed) { 
-                    localStorage.setItem('accessToken', accessToken);
-                    window.location.href = '/'; 
-                }
-            }, 500);
-            
-        } else {
-            // Tratar erro ou apenas fechar após alguns segundos
-            console.error("Nenhum token encontrado na URL de retorno.");
-            // Opcional: window.close(); após timeout
+        const authChannel = new BroadcastChannel('auth_channel');
+        const message: ISocialLoginCallbackEvent = {
+            type: 'GOOGLE_LOGIN_SUCCESS',
+            response: {
+                access_token: accessToken,
+                expires_in: expiresIn ? parseInt(expiresIn) : 3600,
+                scope: scope || 'read write',
+                token_type: tokenType || 'Bearer',
+            } as ILoginUserResponse,
         }
+        
+        authChannel.postMessage(message);
+        authChannel.close();
+        // Fallback & Cleanup
+        setTimeout(() => {
+            window.close();
+            if (!window.closed) { 
+                localStorage.setItem('accessToken', accessToken);
+                window.location.href = '/'; 
+            }
+        }, 500);
+            
     }, [searchParams]);
 
     return (
